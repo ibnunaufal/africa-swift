610a3feb287c831cbef9387841b8ca1e48ea830c
ibnu naufal <ibnunaufal5758@gmail.com>
2023-03-12 11:32:47 +0700

    Initial Commit

---
+++ Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Macbook Pro on 12/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
