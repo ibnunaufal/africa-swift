0a76794685a6c408a7fd3f0dc048b578e0d94911
ibnu naufal <ibnunaufal5758@gmail.com>
2023-03-12 13:06:26 +0700

    n

---
+++ Africa/Model/CoverImageModel.swift
@@ -0,0 +1,13 @@
+//
+//  CoverImageButton.swift
+//  Africa
+//
+//  Created by Macbook Pro on 12/03/2023.
+//
+
+import SwiftUI
+
+struct CoverImage: Codable, Identifiable {
+    let id: Int
+    let name: String
+}
